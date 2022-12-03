87d636d9a30a3c1029e37a5592b3cf8e3c0a92f1
Martin Masevski <martin.masevski@gmail.com>
2022-12-03 16:17:46 +0100

    chore: using ts for tailwindconfig

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        "green-aov": "#42b883",
+        "gray-dark": "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
