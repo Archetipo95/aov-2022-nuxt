5e21cf0c45375aad3d8b9989837ea99975f3b4e1
Martin Masevski <martin.masevski@gmail.com>
2022-12-04 12:59:27 +0100

    feat: improved day-1

--- components/Day01/SearchBar.vue
+++ components/Day01/SearchBar.vue
@@ -1,38 +1,60 @@
 <script setup lang="ts">
+const DEBOUNCE_TIME = 300;
+const isLoading = ref(false);
+const giftSearchBar = ref();
+
 const searchTerm = ref("");
-const products = ref();
+const products = ref([]);
 
 const findProducts = async (term: string) => {
-  if (!term) {
-    products.value = null;
-    return;
-  }
+  if (term.length === 0) return (products.value = []);
+  isLoading.value = true;
+  try {
+    const res = await fetch(
+      `https://dummyjson.com/products/search?q=${term}&limit=5`
+    );
+    const data = await res.json();
+    products.value = data.products;
+  } catch (error) {
+    console.error(error);
+    alert("Something went wrong!");
+  } finally {
+    isLoading.value = false;
 
-  const res = await fetch(`https://dummyjson.com/products/search?q=${term}`);
-  const data = await res.json();
-  products.value = data.products;
+    await nextTick;
+    giftSearchBar.value.focus();
+  }
 };
 
 const debouncedFn = useDebounceFn(() => {
   findProducts(searchTerm.value.trim());
-}, 1000);
+}, DEBOUNCE_TIME);
 
-watch(searchTerm, () => {
-  debouncedFn();
-});
+watch(searchTerm, () => debouncedFn());
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input
+      ref="giftSearchBar"
       type="text"
-      class="p-2 border-2 border-gray-dark text-black"
+      class="py-2 px-4 border-2 border-gray-dark text-black rounded-lg"
       v-model="searchTerm"
       placeholder="Start typing..."
+      :disabled="isLoading"
+      autofocus
     />
-    <ul class="list-disc">
-      <li v-for="{ id, title } in products" :key="id">{{ title }}</li>
+
+    <p v-if="searchTerm && products.length === 0 && !isLoading">
+      No results...
+    </p>
+    <p v-if="isLoading">Please wait...</p>
+
+    <ul>
+      <li v-for="{ id, title, price } in products" :key="id">
+        {{ title }} - ${{ price }}
+      </li>
     </ul>
   </div>
 </template>
