ae13d0a290adcfdd73018cd8307c5dbe90097e7d
Martin Masevski <martin.masevski@gmail.com>
2023-02-08 19:13:45 +0100

    feat: complete day05

--- components/Day05/GiftLabel.vue
+++ components/Day05/GiftLabel.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const from = ref();
+const to = ref();
+</script>
 
 <template>
   <div class="w-full h-full flex justify-center items-center">
@@ -18,10 +21,28 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">
+              FROM: {{ from }}
+            </div>
           </div>
         </div>
+      </div>
+
+      <div
+        class="
+          mt-4
+          gap-2
+          flex flex-col
+          [&>input[type=text]]:text-black
+          [&>input[type=text]]:p-1
+          [&>input[type=text]]:rounded-[4px]
+        "
+      >
+        <label for="to_input">TO:</label>
+        <input type="text" v-model="to" name="to_input" />
+        <label for="from_input">From:</label>
+        <input type="text" v-model="from" name="from_input" />
       </div>
     </div>
   </div>
