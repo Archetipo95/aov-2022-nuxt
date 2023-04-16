96b875705b1016ab1958b2a699d9db220a7be1e4
Martin Masevski <martin.masevski@gmail.com>
2023-04-16 18:38:55 +0200

    chore: updated some deps

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,29 @@ const pages = [
   { title: "Day 04: Recursive Tree", path: "day-04" },
   { title: "Day 05: Gift Label Maker", path: "day-05" },
   { title: "Day 06: Price Comparator", path: "day-06" },
-  { title: "Day 07: Drag Gifts", path: "day-07" },
+  // { title: "Day 07: Drag Gifts", path: "day-07" },
 ];
 </script>
 
 <template>
   <ul class="grid grid-cols-4 gap-4">
     <li v-for="{ title, path } in pages" :key="path">
-      <nuxt-link :to="`/${path}`"
-        class="border h-[180px] w-[170px] rounded-lg p-5 flex justify-center items-center hover:bg-white hover:text-gray-dark transition-all">
+      <nuxt-link
+        :to="`/${path}`"
+        class="
+          border
+          h-[180px]
+          w-[170px]
+          rounded-lg
+          p-5
+          flex
+          justify-center
+          items-center
+          hover:bg-white
+          hover:text-gray-dark
+          transition-all
+        "
+      >
         {{ title }}
       </nuxt-link>
     </li>
